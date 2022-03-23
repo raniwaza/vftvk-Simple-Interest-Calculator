9eeb19e1affe0e9083a1a0f466ce303110470c2c
rani azd <raniwaza@gmail.com>
2022-03-23 14:24:50 +0000

    final editing opefully

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
-    if (p<1){
-            alert("please insert a greater than 0");
-           document.getElementById("principal").focus();
-             }
+  
+  if (p<1){alert("please insert a greater than 0");
+          document.getElementById("principal").focus();}
   else{
     r = document.getElementById("rate").value;
     t = document.getElementById("years").value;
     result = p*(1+(r*t)/100)-p;
   
  y1 =  Number(t) + 2022;       
-document.getElementById("result").innerHTML = "if you deposit "+ p +"<br> at an interest rate of "+r+"% <br> "
-+"You will receive an amount of "+ result +" in the year " + y1;
-return 0;
-}
+document.getElementById("result").innerHTML = "if you deposit <mark>"+ p +"</mark><br> at an interest rate of <mark>"+r+" %</mark><br> "
++"You will receive an amount of <mark>"+ result +"</mark>,<br/> in the year <mark>" + y1+"</mark>";
+return 0;}
 }
  
