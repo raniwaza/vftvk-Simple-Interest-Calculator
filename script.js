ca544671215f221742d953e6e09fdcce30f236d6
rani azd <raniwaza@gmail.com>
2022-03-23 12:53:19 +0000

    changes for the project

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    result = p*(1+(r*t)/100)-p;
+  
+ y1 =  Number(t) + 2022;       
+document.getElementById("result").innerHTML = "if you deposit "+ p +"<br> at an interest rate of "+r+"% <br> "
++"You will receive an amount of "+ result +" in the year " + y1;
+return 0;
 }
-        
+ 
